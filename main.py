ff93e36032062a74b855b8d236d4920c6c714889
Abdulrahman Jomaa <abdulrahman.jomaa@ulb.be>
2026-04-09 13:35:59 +0200

    Experimenting and getting data

--- main.py
+++ main.py
@@ -1,13 +1,47 @@
 from modelBuilder import ModelBuilder
+import time
+import csv
+
+
+def write_to_csv(data):
+    for i, matrix in enumerate(data):
+        filename = f"Data_P{i + 1}.csv"
+
+        with open(filename, "w", newline="") as f:
+            writer = csv.writer(f)
+            writer.writerows(matrix)
 
 
 if __name__ == '__main__':
-    model = ModelBuilder("model.json").build()
-    model.optimize()
+    problems_path = ["p1.json", "p2.json", "p3.json"]
+    REPETITIONS = 200
+    elapsed_times_4_all_problems = []
 
-    print("Status:", model.getStatus())
+    print("Starting the experiment : ")
 
-    for v in model.getVars():
-        val = model.getVal(v)
-        if abs(val) > 1e-6:
-            print(v.name, val)
+    for p_path in problems_path:
+        m = [["Solving Time", "Building Time", "Total Time"]]
+        for _ in range(REPETITIONS):
+            # building time
+            start = time.perf_counter()
+            model = ModelBuilder(p_path).build()
+            end = time.perf_counter()
+            building_elapsed_time_us = (end - start) * 1e6
+
+            model.hideOutput()
+
+            # solving time
+            start = time.perf_counter()
+            model.optimize()
+            end = time.perf_counter()
+
+            solving_elapsed_time_us = (end - start) * 1e6
+            m.append([solving_elapsed_time_us, building_elapsed_time_us,
+                      building_elapsed_time_us + solving_elapsed_time_us])
+
+        elapsed_times_4_all_problems.append(m)
+
+        print("Problem " + p_path + " done.")
+
+    write_to_csv(elapsed_times_4_all_problems)
+    print("Saved All data in csv files.")
